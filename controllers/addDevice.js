db6ffe78d94f2f82e91edfa20e07dc87bffd5511
Pham Duy Thuc <thucphamduy182@gmail.com>
2023-08-17 08:30:08 +0700

    update code

--- controllers/addDevice.js
+++ controllers/addDevice.js
@@ -28,6 +28,7 @@ export const handOver = async function (req, res, next) {
             device.status = 1
         }
         await device.save()
+        console.log(device)
         res.status(201).json({
             message: 'success',
             response: device.allotment
@@ -41,7 +42,7 @@ export const handOver = async function (req, res, next) {
 }
 export const updateDevice = async function (req, res, next) {
     try {
-        const device = await deviceModel.findByIdAndUpdate(req.params.id,  req.body)
+        const device = await deviceModel.findByIdAndUpdate(req.params.id, req.body)
         res.status(200).json({
             message: 'success',
             response: device
@@ -55,10 +56,17 @@ export const updateDevice = async function (req, res, next) {
 }
 export const getDevice = async function (req, res, next) {
     try {
-        const device = await deviceModel.find();
+        const device = await deviceModel.find().lean().exec();
+        console.log(device)
+        const sanitizedData = device.map(item => {
+            const {_id, ...rest} = item;
+
+            return {id: _id, ...rest};
+        });
+
         res.status(200).json({
             message: 'success',
-            response: device
+            response: sanitizedData
         })
     } catch (error) {
         res.status(400).json({
@@ -67,14 +75,32 @@ export const getDevice = async function (req, res, next) {
     }
 
 }
-export  const deleteItemDevice = async function (req, res, next) {
+
+export const getDetailDevice = async function (req, res, next) {
+    try {
+        const detailService = deviceModel.findById(req.params.id);
+        res.status(200).json({
+            code: '00',
+            message: 'success',
+            response: detailService
+        })
+    } catch (e) {
+        res.status(400).json({
+            message: 'fail'
+
+        })
+    }
+
+};
+
+export const deleteItemDevice = async function (req, res, next) {
     try {
         const item = await deviceModel.deleteOne({_id: req.params.id})
         res.status(200).json({
             message: 'success',
             response: item
         })
-    }catch (error) {
+    } catch (error) {
         console.log(error)
         res.status(400).json({
             message: 'fail',
