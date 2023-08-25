aa38c92f25c740007b7632027d393f84baa9ac13
thuc duy <thucphamduy182@gmail.com>
2023-08-25 16:35:46 +0700

    update code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import router from './routers/device.js'
+import * as path from "path";
 // const  morgan = require('morgan');
 import cors from 'cors'
 
@@ -10,7 +11,7 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 app.use('/api/v1', router);
 
